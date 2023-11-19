e35cf430d66154f1c4d3665188e7eb3095539ed7
guolinhome <1226618259@qq.com>
2023-11-19 14:08:46 +0800

    更新

--- api/test.php
+++ api/test.php
@@ -1,7 +1,7 @@
 <?php
 
-$pattern = $_GET['pattern'] ?? '*.*';
+// $pattern = $_GET['pattern'] ?? '*.*';
 
-foreach (glob($pattern) as $filename) {
-    echo "$filename size " . filesize($filename) . "\n";
-}
+// foreach (glob($pattern) as $filename) {
+//     echo "$filename size " . filesize($filename) . "\n";
+// }
